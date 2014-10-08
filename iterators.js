f8b4002e72bfaa0038d73e28f781f2041e578d7e
Switch-Thomas <admiralswitch@gmail.com>
2014-10-08 09:12:12 -0700

    half way there

--- iterators.js
+++ iterators.js
@@ -1,13 +1,57 @@
 var iterators = {
-    max: function (numList) {
-      var max = -Infinity;
-      for (var i = 0; i < numList.length; i++) {
+  max: function (numList) {
+    var max = -Infinity;
+    for (var i = 0; i < numList.length; i++) {
         if (numList[i] > max) {
           max = numList[i];
         }
-      }
-      return max;
     }
-  };
+    return max;
+  },
+
+  min: function (numList) {
+    var min = Infinity;
+    for (var i = 0; i < numList.length; i++) {
+      if (numList[i] < min) {
+        min = numList[i];
+      }
+    }
+    return min;
+  },
+
+  each: function(items,myFunction) {
+      for (var i =0; i < items.length; i++) {
+        myFunction(items[i]);
+      }
+  },
+
+  // each: function (numList, func) {
+  //   var output = [];
+  //   for (var i = 0; i < numList.length; i++) {
+  //     output = numList[i] + 1;
+  //   } 
+  //   return output;
+  // },
+   
+//   map: function (numList, func) {
+//     var output = [];
+//       for (var i = 0; i < numList.length; i++) {
+//         // func(numList[i] + 1);
+//         var chicken = func(numList[i] + 1);
+//         output.push(func());
+//     } 
+//     return output;
+//   },
+// };
+map: function (myArray, callback){
+  var newArray = [];
+  for (var i = 0; i < myArray.length; i++) {
+    newArray.push(callback(myArray[i]));
+  }
+  return newArray;
+}
+
+};
+
 
 module.exports = iterators;
--- spec/iterators_test.js
+++ spec/iterators_test.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var Iterators = require('../iterators.js');
+var Iterators = require('../iterators');
 
 describe('Iterators', function () {
   describe('#max', function () {
@@ -20,5 +20,109 @@ describe('Iterators', function () {
       myArr = [];
       expect(Iterators.max(myArr)).to.equal(-Infinity);
     });
+  });
+});
+
+describe('Iterators', function () {
+  describe('#min', function () {
+    var myArr;
+    beforeEach(function () {
+      myArr = [66, 22, 67, 34];
+    });
+
+    it('should return the minimum in an array', function () {
+      expect(Iterators.min(myArr)).to.equal(22);
+    });
+    it('should return -Infinity for empty array', function () {
+      myArr = [];
+      expect(Iterators.min(myArr)).to.equal(Infinity);
+    });
+  });
+});
+
+
+describe('Iterators', function () {
+  describe('#each', function () {
+    var myArr;
+    beforeEach(function () {
+      myArr = [66, 22, 67, 34];
+    });
+
+  it("should return an action to each value in array", function() {
+    var sum = 0;
+    Iterators.each(myArr, function (item){
+      sum += item; 
+    }
+  }
+  expect(sum).to.equal(6);
+});
+
+// describe('Iterators', function () {
+//   describe('Each', function () {
+//     var myArr;
+//     var thingshappen = function(){
+
+//     };
+//     beforeEach(function () {
+//       myArr = [66, 22, 67, 34];
+//     });
+//     it('Executes a provided function once per array element', function () {
+//       expect(Iterators.each(myArr, thingshappen)).to.equal(67);
+//     });
+//   });
+// });
+
+// describe('Iterators', function () {
+//   describe('Map', function () {
+//     var myArr;
+//     var addOne = function(a){
+//       a = a + 1;
+//       return a;
+//     };
+
+//     beforeEach(function () {
+//       myArr = [66, 22, 67, 34];
+//     });
+//     it('should add 1 to each element of the array', function () {
+//       expect(Iterators.map(myArr, addOne)).to.equal([67, 23, 68, 35]);
+//     });
+//   });
+// });
+
+ // map test
+describe("map", function(){
+  var myArr;
+    beforeEach(function () {
+    myArr = [66, 22, 67, 34];
+  });
+
+  var timesTwo = function(x){
+    return x+1;
+  };
+
+  it("should create a new array of values*2", function(){
+  // Test starts here, call map function
+    var result = Iterators.map(myArr, timesTwo);
+
+    // now compare result with expected
+    expect(result).to.deep.equal([67, 23, 68, 35]);
+    });
+});
+
+describe("filter", function(){
+  it("it should ...", function(){
+    
+  });
+});
+
+describe("reduce", function(){
+  it("it should ...", function(){
+    
+  });
+});
+
+describe("reject", function(){
+  it("it should ...", function(){
+    
   });
 });
